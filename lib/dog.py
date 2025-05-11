ec735558ae2813d29ea4077569bccefd49a8a03d
happiness <happinesskhalan@gmail.com>
2025-05-11 17:41:30 +0300

    attributes and properties concept complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,31 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+   
+    def __init__(self, name="Buddy", breed="Pug"):
+        # Public attributes that tests will access
+        self.name = None
+        self.breed = None
+        
+        # Call setters to validate and set values
+        self.set_name(name)
+        self.set_breed(breed)
+        
+    def set_name(self, value):
+       
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self.name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def set_breed(self, value):
+       
+        # Create a case-insensitive comparison
+        approved_breeds_lower = [breed.lower() for breed in APPROVED_BREEDS]
+        
+        if isinstance(value, str) and value.lower() in approved_breeds_lower:
+            # Find the correctly capitalized version
+            index = approved_breeds_lower.index(value.lower())
+            self.breed = APPROVED_BREEDS[index]
+        else:
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    
+    def __init__(self, name ="Penninah", job ="Sales"):
+        self.name = None
+        self.job = None
+        self.set_name(name)
+        self.set_job(job)
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1<= len(value) <= 25 :
+            self.name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def set_job(self, value):
+        
+        if value in APPROVED_JOBS:
+            self.job = value
+        else:
+            print("Job must be in list of approved jobs.")
+        
+
+
+    
+        
